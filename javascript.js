2481a586e9849f27c1bc3ce5320ead0460527b50
keenoo <90801700+keenooo@users.noreply.github.com>
2023-10-13 23:01:18 +0200

    Allow size of container to scale to window size
    Size of container is now dependant on window size, and box flex values
    change accordingly, except for when window is changed after board has
    already been initialized.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,21 @@
 const initGridSize = 16;
-const containerSize = 200;
+const containerSize = 80;
 
 const container     = document.querySelector(".container");
 const newBoardBtn   = document.querySelector(".new-board-btn");
 
-container.style.width = containerSize + "px";
-container.style.height = containerSize + "px";
+container.style.width = containerSize + "vh";
+container.style.height = containerSize + "vh";
 
 newBoardBtn.addEventListener('click', () => newBoardBtnPress())
 
-
 function createBoard(gridSize) {
+    const containerStyle = window.getComputedStyle(container);
+    const containerWidth = containerStyle.getPropertyValue("width").match(/\d+/);
+    console.log(containerWidth);
+
     for (let i = 0; i < gridSize * gridSize; i++) {
-        const box = createBox(gridSize);
+        const box = createBox(gridSize, containerWidth);
     
         const b = container.appendChild(box);
     }
@@ -22,19 +25,18 @@ function removeBoard() {
     container.innerHTML = "";
 }
 
-function createBox(gridSize) {
+function createBox(gridSize, containerWidth) {
     const box = document.createElement('div');
     box.classList.add("box");
     box.classList.add("unselectedBox");
 
-    box.style.flex = "1 1 " + containerSize / gridSize + "px" ; 
+
+    box.style.flex = "1 1 " + containerWidth / gridSize + "px" ; 
 
     box.addEventListener("mouseover", () => box.classList.add("selected-box"));
 
     return box;
 }
-
-
 
 function newBoardBtnPress() {
     removeBoard();
