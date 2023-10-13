f93144929c3010161dab78b5d999fa45a7f6b2d5
keenoo <90801700+keenooo@users.noreply.github.com>
2023-10-13 22:51:45 +0200

    Add button to create new board with arbitraty size
    DOM is created with a button which handles creating new boards and
    cleaning it up. JS code has been cleaned up and handles all size
    variables instead of CSS. Dynamically updates flex attributes to
    properly scale boxes to set board size.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,47 @@
-const gridSize = 16;
+const initGridSize = 16;
+const containerSize = 200;
 
-const container = document.querySelector(".container");
+const container     = document.querySelector(".container");
+const newBoardBtn   = document.querySelector(".new-board-btn");
 
-let boxes = [];
+container.style.width = containerSize + "px";
+container.style.height = containerSize + "px";
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const box = createBox();
+newBoardBtn.addEventListener('click', () => newBoardBtnPress())
 
-    const b = container.appendChild(box);
 
-    boxes.push(b);
+function createBoard(gridSize) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const box = createBox(gridSize);
+    
+        const b = container.appendChild(box);
+    }
 }
 
-function createBox() {
+function removeBoard() {
+    container.innerHTML = "";
+}
+
+function createBox(gridSize) {
     const box = document.createElement('div');
     box.classList.add("box");
     box.classList.add("unselectedBox");
+
+    box.style.flex = "1 1 " + containerSize / gridSize + "px" ; 
 
     box.addEventListener("mouseover", () => box.classList.add("selected-box"));
 
     return box;
 }
+
+
+
+function newBoardBtnPress() {
+    removeBoard();
+
+    const gridSize = prompt("Choose gird-size:");
+
+    createBoard(gridSize);
+}
+
+createBoard(initGridSize);
