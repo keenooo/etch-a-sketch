0fa450858ac1fc291c2f3ee63a7e72c8e679920c
keenoo <90801700+keenooo@users.noreply.github.com>
2023-10-13 22:13:44 +0200

    Create box-adding function with hover effect
    The box-adding logic has been moved to a seperate function and now
    contains eventListeners to add the class "selectedBox" to boxes that
    are currently being hovered over.

--- javascript.js
+++ javascript.js
@@ -5,11 +5,20 @@ const container = document.querySelector(".container");
 let boxes = [];
 
 for (let i = 0; i < gridSize * gridSize; i++) {
-    const box = document.createElement('div');
-    box.classList.add("box");
-    box.classList.add("unselectedBox");
+    const box = createBox();
 
     const b = container.appendChild(box);
 
     boxes.push(b);
+}
+
+function createBox() {
+    const box = document.createElement('div');
+    box.classList.add("box");
+    box.classList.add("unselectedBox");
+
+    box.addEventListener("mouseover", () => box.classList.add("selectedBox"));
+    box.addEventListener("mouseout", () => box.classList.remove("selectedBox"));
+
+    return box;
 }
